b73c789a791339e66c176790bd769e079146920d
Dani Muñoz <davimufo99@gmail.com>
2025-10-16 16:37:34 +0200

    upd

---
+++ Ejercicios Tema 1/src/Ejercicio1.java
@@ -0,0 +1,15 @@
+public class Ejercicio1 {
+
+    static void main(String[] args) {
+        String nombre = "Dani";
+        int edad = 26;
+        String ciudad = "Valencia";
+
+        System.out.println("Mi nombre es: "+nombre);
+        System.out.println();
+        System.out.println("Edad: "+edad);
+        System.out.println();
+        System.out.println("Vivo en: "+ciudad);
+        System.out.println("a ver si ya por fin puedes funcionar hijo de la gran putisisimamasmdsamasm");
+    }
+}
